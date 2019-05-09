21e87f5d007b3f3bb8904d796a09c0b4ecb02d0e
Gordon Strodel <gordonstrodel@gmail.com>
2019-05-08 21:57:51 -0400

    Updated code to the correctly working copy

--- Predict.sql
+++ Predict.sql
@@ -1,26 +1,20 @@
 #standardSQL
-select 
---MIN(predicted_timestamp) MIN_ETA_for_soil_dryness 
---MAX(predicted_timestamp) ETA_for_soil_dryness
-MAX(TIMESTAMP(FORMAT_TIMESTAMP('%F %T', predicted_timestamp, "America/New_York"))) as ETA_for_soil_dryness
-FROM (
 SELECT
-CAST(predicted_UNIX_DATETIME as INT64) as predicted_UNIX_DATETIME
-, TIMESTAMP_SECONDS(CAST(predicted_UNIX_DATETIME as INT64)) as predicted_timestamp
-, UNIX_DATETIME
-, raw_soil_value
+TIMESTAMP_SECONDS(CAST(UNIX_DATETIME as INT64)+1556409605) as timestamp
+, round(predicted_raw_soil_value,2) as predicted_raw_soil_value
 FROM
-  ML.PREDICT(MODEL plant_sensor.linear_model_timestamp,
+  ML.PREDICT(MODEL plant_sensor.linear_model,
     (
-      SELECT
-      UNIX_SECONDS( TIMESTAMP(FORMAT_TIMESTAMP('%F %T', human_readable_date, "America/New_York")))  as UNIX_DATETIME
-      ,avg(soil) as raw_soil_value
-      FROM `smartplant31.plant_sensor.raw_sensor_data` 
+     select 
+     shift_value,
+     UNIX_SECONDS( TIMESTAMP(FORMAT_TIMESTAMP('%F %T', shift_value, "America/New_York")))-1556409605  as UNIX_DATETIME
+     from plant_sensor.date_time_table
       WHERE 1=1
-       --and FORMAT_TIMESTAMP('%F %T', human_readable_date, "America/New_York") >='2019-05-01 00:00:00'
-       and soil >= 2900
-      GROUP BY  human_readable_date
-      ORDER BY human_readable_date DESC
+     and shift_value >='2019-05-01 00:00:00'
+     and shift_value <'2019-05-15 00:00:00'
+     GROUP BY shift_value
+     ORDER BY shift_value ASC
      )
   )
-)
+where predicted_raw_soil_value >=2900  
+and predicted_raw_soil_value <= 3100
